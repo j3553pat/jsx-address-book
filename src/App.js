e65bc7000fc12bf4dc1dd60ef4a8573e610476e5
j3553pat <jessecodedev@gmail.com>
2023-05-15 20:25:31 -0500

    add a map state to fetch data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import ProfileInfo from "./profileCard.js";
+const Url = "https://randomuser.me/api?results=25"
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrayOfProfile: [],
+      isLikes: false,
+    };
+  }
+  componentDidMount() {
+    axios.get(Url)
+    .then( res => {
+      const arrayOfProfile = res.data
+      this.setState({arrayOfProfile})
+    })
+  };
+
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{this.state.arrayOfProfile.map((profile, index) => {
+          return (
+            <ProfileInfo
+             key= {index} 
+             name={profile.name}
+             />
+          )
+        })}</p>
       </header>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
